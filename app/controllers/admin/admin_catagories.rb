8e1b3f1e5738a39cd669e050b3716ab52889badc
tarikyavuzca <tarikyavuzca@gmail.com>
2021-11-12 01:06:40 -0500

    creating and adding admin_categories

--- app/controllers/admin/admin_catagories.rb
+++ app/controllers/admin/admin_catagories.rb
@@ -1,3 +1,19 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   @categories = Category.order(:name).all
+end
+
+def new
+  @category = Category.new
+end
+
+def create 
+    @category = Category.new(params.require(:category).permit(:name))
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 end
